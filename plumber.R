70c4b84e796147530bba8665f55c5b4cde3b0d01
Nick Twort <Nick.Twort@houstonkemp.com>
2022-06-03 15:08:50 +1000

    Initial commit

---
+++ plumber.R
@@ -0,0 +1,39 @@
+#
+# This is a Plumber API. In RStudio 1.2 or newer you can run the API by
+# clicking the 'Run API' button above.
+#
+# In RStudio 1.1 or older, see the Plumber documentation for details
+# on running the API.
+#
+# Find out more about building APIs with Plumber here:
+#
+#    https://www.rplumber.io/
+#
+
+library(plumber)
+library(ggplot2)
+
+#* @apiTitle Plumber Example API
+
+#* Plot a histogram
+#* @serializer svg
+#* @get /plot
+function(){
+  col <- runif(3)
+  
+  colour <- rgb(col[1], col[2], col[3])
+  
+  t <- seq.int(0, 6280, 10)/1000
+  
+  a <- 3
+  x <- 2 * a * (1 - cos(t)) * cos(t)
+  y <- 2 * a * (1 - cos(t)) * sin(t)
+  tbl <- data.frame(x, y)
+  
+  g <- ggplot(tbl, aes(x, y)) +
+    geom_polygon(fill = colour) +
+    theme_void() +
+    theme(panel.background = element_blank(), plot.background = element_blank())
+  
+  print(g)
+}
