625f131036a06a34bdb3f52b4a96b56730b03709
Nick Twort <nicholas.twort@gmail.com>
2022-12-07 13:17:38 +1100

    Test this for getting the right filepath

--- scheduled_downloads.R
+++ scheduled_downloads.R
@@ -1,17 +1,19 @@
-library(here)
+frame_files <- lapply(sys.frames(), function(x) x$ofile)
+frame_files <- Filter(Negate(is.null), frame_files)
+PATH <- dirname(frame_files[[length(frame_files)]])
 download.file(
   "https://docs.google.com/spreadsheets/d/1T9FPgB5FhqqS8wuWbGcGY-l9M7QZDIHY8-BORKXgo5o/export?gid=915433531&format=csv",
-  here("data/distance_to_go.csv")
+  file.path(PATH, "data/distance_to_go.csv")
 )
 download.file(
   "https://docs.google.com/spreadsheets/d/1bOpX7ynvyUR4cuTmenj8ZQ-9UHv_ekYdovGUhP-YCuo/export?gid=395516908&format=csv",
-  here("data/wedding_summary.csv")
+  file.path(PATH, "data/wedding_summary.csv")
 )
 download.file(
   "https://docs.google.com/spreadsheets/d/1T9FPgB5FhqqS8wuWbGcGY-l9M7QZDIHY8-BORKXgo5o/export?gid=1956137224&format=csv",
-  here("data/running.csv")
+  file.path(PATH, "data/running.csv")
 )
 download.file(
   "https://docs.google.com/spreadsheets/d/1T9FPgB5FhqqS8wuWbGcGY-l9M7QZDIHY8-BORKXgo5o/export?gid=888983572&format=csv",
-  here("data/cycling.csv")
+  file.path(PATH, "data/cycling.csv")
 )
